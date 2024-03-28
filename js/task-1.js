e2cbc54a5f10714567295f77d21432d967f9afc9
andrii-cabbage <kapysta377@gmail.com>
2024-03-28 21:04:29 +0200

    hw

---
+++ js/task-1.js
@@ -0,0 +1,44 @@
+function isEnoughCapacity(products, containerSize) {
+    
+    let totalProducts = 0;
+  
+    
+    for (let product in products) {
+      
+      if (products.hasOwnProperty(product)) {
+        
+        if (typeof products[product] === 'number') {
+          
+          totalProducts += products[product];
+        } else {
+          
+          console.error('Количество товара должно быть числом.');
+          return false;
+        }
+      }
+    }
+  
+    
+    if (totalProducts <= containerSize) {
+      return true; 
+    } else {
+      return false; 
+    }
+  }
+  
+  
+  console.log(
+    isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)
+  ); // true
+  
+  console.log(
+    isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)
+  ); // false
+  
+  console.log(
+    isEnoughCapacity({ apples: 1, lime: 5, tomatoes: 3 }, 14)
+  ); // true
+  
+  console.log(
+    isEnoughCapacity({ apples: 18, potatoes: 5, oranges: 2 }, 7)
+  ); // false
